6867f39abf16042a1c652c6a17bbcfbb1b49c9f0
klausm <simon.klausner@swarovski.com>
2021-12-21 13:29:42 +0100

    update to use my own implementation using Game class

--- src/main/kotlin/day21.kt
+++ src/main/kotlin/day21.kt
@@ -1,74 +1,118 @@
 import java.io.File
 import kotlin.math.max
 
-fun main() {
-    abstract class Die(var value: Int) {
-        abstract fun roll(): Int
-    }
+interface Die {
+    fun roll(): Int
+}
 
-    class DeterministicDie : Die(0) {
-        override fun roll(): Int {
-            value++
-            if (value > 100) {
-                value = 1
-            }
-            return value
+class DeterministicDie : Die {
+    private var value = 0
+
+    override fun roll(): Int {
+        value++
+        if (value > 100) {
+            value = 1
         }
+        return value
     }
+}
 
-    class FixedDie(private val values: Array<Int>) : Die(0) {
-        private var currentIdx = 0
+class FixedDie(private val values: IntArray) : Die {
+    private var currentIdx = 0
 
-        override fun roll(): Int {
-            val result = values[currentIdx]
-            currentIdx = (currentIdx + 1) % values.size
-            return result
+    override fun roll(): Int {
+        val result = values[currentIdx]
+        currentIdx = (currentIdx + 1) % values.size
+        return result
+    }
+}
+
+data class Game(val players: Array<Int>, val die: Die, val goal: Int) {
+    private var numDiceThrows = 0
+    val scores = IntArray(players.size)
+    var currentPlayer = 0
+
+    val winningPlayer: Int
+        get() = if (scores[0] > scores[1]) 0 else 1
+
+    private val losingPlayer: Int
+        get() = if (scores[0] > scores[1]) 1 else 0
+
+    val losingScore: Int
+        get() = scores[losingPlayer] * numDiceThrows
+
+    fun isFinished(): Boolean =
+        scores.any { it >= goal }
+
+    fun turn() {
+        var totalAmount = 0
+        repeat(3) {
+            totalAmount += die.roll()
+            ++numDiceThrows
         }
+
+        var playerLoc = players[currentPlayer] + totalAmount
+        while (playerLoc > 10) {
+            playerLoc -= 10
+        }
+        players[currentPlayer] = playerLoc
+
+        scores[currentPlayer] += players[currentPlayer]
+        currentPlayer = (currentPlayer + 1) % players.size
     }
 
-    data class Game(val players: Array<Int>, val die: Die, val goal: Int) {
-        var numDiceThrows = 0
-            private set
-        private val scores = IntArray(players.size)
-        private var currentPlayer = 0
-
-        fun copyWithDie(die: Die): Game {
-            return Game(this.players, die, this.goal).apply {
-                this.numDiceThrows = this@Game.numDiceThrows
-                this@Game.scores.forEachIndexed { idx, score ->
-                    this.scores[idx] = score
+    companion object {
+        fun ofState(state: State, rolls: IntArray, goal: Int): Game {
+            return Game(state.players.clone(), FixedDie(rolls), goal).apply {
+                state.scores.forEachIndexed { idx, value ->
+                    this.scores[idx] = value
                 }
-                this.currentPlayer = this@Game.currentPlayer
+                this.currentPlayer = state.currentPlayer
             }
-        }
-
-        val losingPlayer: Int
-            get() = if (scores[0] > scores[1]) 1 else 0
-
-        val losingScore: Int
-            get() = scores[losingPlayer] * numDiceThrows
-
-        fun isFinished(): Boolean =
-            scores.any { it >= goal }
-
-        fun turn() {
-            var totalAmount = 0
-            repeat(3) {
-                totalAmount += die.roll()
-                ++numDiceThrows
-            }
-
-            var playerLoc = players[currentPlayer] + totalAmount
-            while (playerLoc > 10) {
-                playerLoc -= 10
-            }
-            players[currentPlayer] = playerLoc
-
-            scores[currentPlayer] += players[currentPlayer]
-            currentPlayer = (currentPlayer + 1) % players.size
         }
     }
+}
 
+data class State(
+    val players: Array<Int>,
+    val scores: IntArray,
+    val currentPlayer: Int,
+    val winningPlayer: Int
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as State
+
+        if (!players.contentEquals(other.players)) return false
+        if (!scores.contentEquals(other.scores)) return false
+        if (currentPlayer != other.currentPlayer) return false
+        if (winningPlayer != other.winningPlayer) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = players.contentHashCode()
+        result = 31 * result + scores.contentHashCode()
+        result = 31 * result + currentPlayer
+        result = 31 * result + winningPlayer
+        return result
+    }
+
+    companion object {
+        fun ofGame(game: Game): State {
+            return if (game.isFinished()) {
+                State(game.players.clone(), game.scores.clone(), game.currentPlayer, game.winningPlayer)
+            } else {
+                State(game.players.clone(), game.scores.clone(), game.currentPlayer, -1)
+            }
+        }
+    }
+}
+
+fun main() {
     val lines = File("inputs/day21.txt").readLines()
     val p1 = lines[0].substringAfter(":").trim().toInt()
     val p2 = lines[1].substringAfter(":").trim().toInt()
@@ -81,77 +125,32 @@ fun main() {
         println(game.losingScore)
     }
 
-    /**
-     * Solution of Part 2 is not by me
-     * It is from here: https://github.com/Kroppeb/AdventOfCodeSolutions/blob/a0b0ad018b07325bf499103b3de9e46048f864fa/src/day%2021.kt
-     *
-     * Added it for completion of this day.
-     *
-     * I couldn't find a proper way to use my Game class to simulate all possible outcomes.
-     * Tried to find a way doing it one by one for every possible universe combination but in the end I gave up because
-     * I couldn't come up with a nice solution.
-     */
-    data class State(val p1: Int, val p2: Int, val scoreP1: Int, val scoreP2: Int)
-
     fun part2() {
-        var counter = mutableMapOf(State(p1, p2, 0, 0) to 1L)
+        val states = mutableMapOf(State(arrayOf(p1, p2), IntArray(2), 0, -1) to 1L)
+        while (states.any { it.key.winningPlayer == -1 }) {
+            val iter = states.filter { it.key.winningPlayer == -1 }.iterator()
+            while (iter.hasNext()) {
+                val (state, amount) = iter.next()
 
-        var p1Wins = 0L
-        var p2Wins = 0L
-
-        while (!counter.isEmpty()) {
-            var iterator = counter.iterator()
-
-            val (state, l) = iterator.next()
-            iterator.remove()
-
-            val (p1, p2, scoreP1, scoreP2) = state
-
-            repeat(3) { d1 ->
-                repeat(3) { d2 ->
-                    repeat(3) { d3 ->
-                        var px1 = p1
-                        px1 += d1 + d2 + d3 + 3
-                        px1--
-                        px1 %= 10
-                        px1++
-
-                        var scorePx1 = scoreP1
-
-                        scorePx1 += px1
-                        if (scorePx1 >= 21) {
-                            p1Wins += l
-                        } else {
-                            repeat(3) { dx1 ->
-                                repeat(3) { dx2 ->
-                                    repeat(3) { dx3 ->
-                                        var px2 = p2
-                                        px2 += dx1 + dx2 + dx3 + 3
-                                        px2--
-                                        px2 %= 10
-                                        px2++
-
-                                        var scorePx2 = scoreP2
-                                        scorePx2 += px2
-                                        if (scorePx2 >= 21) {
-                                            p2Wins += l
-                                        } else {
-                                            val state1 = State(px1, px2, scorePx1, scorePx2)
-                                            if (state1 in counter) {
-                                                counter[state1] = l + counter[state1]!!
-                                            } else {
-                                                counter[state1] = l
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                // simulate all possible 27 outcomes of a single turn of one specific universe
+                repeat(3) { roll1 ->
+                    repeat(3) { roll2 ->
+                        repeat(3) { roll3 ->
+                            val game = Game.ofState(state, intArrayOf(roll1 + 1, roll2 + 1, roll3 + 1), 21)
+                            game.turn()
+                            val newState = State.ofGame(game)
+                            val currAmount = states.getOrPut(newState) { 0L }
+                            states[newState] = currAmount + amount
                         }
                     }
                 }
+
+                states.remove(state)
             }
         }
 
+        val p1Wins = states.asIterable().filter { it.key.winningPlayer == 0 }.sumOf { it.value }
+        val p2Wins = states.asIterable().filter { it.key.winningPlayer == 1 }.sumOf { it.value }
         println(max(p1Wins, p2Wins))
     }
 
